e221f6019e21c34708d77076dce5cd0da5840280
umesh035 <152913791+umesh035@users.noreply.github.com>
2023-12-05 00:10:25 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,46 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event InsurancePurchased(uint256 premium);
+    event InsuranceClaimFiled(address indexed owner, uint256 claimAmount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
+        uint256 _previousBalance = balance;
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -48,13 +43,21 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function purchaseInsurance(uint256 _premium) public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(msg.value == _premium, "Incorrect premium amount sent");
+        emit InsurancePurchased(_premium);
+    }
+
+    function fileInsuranceClaim(uint256 _claimAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        emit InsuranceClaimFiled(msg.sender, _claimAmount);
     }
 }
